24e08759db89ec0ecaac40009f0372407c3b936e
matheusgir <girardi.matheus@hotmail.com>
2021-03-11 22:16:53 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState} from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native'
+
+import Slider from '@react-native-community/slider'
 
 export default function App() {
-  return (
+
+  const [password, setPassword] = useState('123456');
+
+  function generatePass(){
+    setPassword('@girardi')
+  }
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image 
+        source={require('./src/assets/logo.png')}
+        style={styles.logo}
+      />
+      <Text style={styles.title}>12 Caracteres</Text>
+      <View style={styles.area}>
+        <Slider 
+          style={{ height: 50 }}
+          minimumValue={5}
+          maximumValue={15}
+          minimumTrackTintColor="#FF0000"
+          maximumTrackColor="#000"
+        />
+      </View>
+
+    <TouchableOpacity style={styles.button} onPress={generatePass}>
+      <Text style={styles.buttonText}>Gerar Senha</Text>
+    </TouchableOpacity>
+
+    <View style={styles.area}>
+      <Text style={styles.password}>{password}</Text>
     </View>
-  );
+
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#F3F3FF'
   },
-});
+  logo: {
+    marginBottom: 60
+  },
+  title:{
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  area: {
+    marginTop: 15,
+    marginBottom: 15,
+    backgroundColor: '#FFF',
+    width: '80%',
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: '#FFA200',
+    width: '80%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 7,
+    marginBottom: 25,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#FFF',
+    fontWeight: 'bold'
+  },
+  password: {
+    padding: 10,
+    textAlign: 'center',
+    fontSize: 20
+  }
+})
